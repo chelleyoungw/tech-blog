1e6ad4b910551fd33733edb76dc0ed8a9a04318d
Chelle Wood <chelleyoungw@gmail.com>
2024-06-14 12:40:31 -0500

    added login utils

---
+++ utils/helpers.js
@@ -0,0 +1,20 @@
+function formatDate(date, format) {
+  const d = new Date(date);
+  let month = "" + (d.getMonth() + 1);
+  let day = "" + d.getDate();
+  const year = d.getFullYear();
+
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [month, day, year].join("/");
+}
+
+function eq(x, y) {
+  return x === y;
+}
+
+module.exports = {
+  formatDate,
+  eq,
+};
